0031101da325a5898eb6b0bb0203d6abdc1c1fdf
pedronaresi <pmnaresi@hotmail.com>
2016-09-20 19:08:28 -0300

    Add files via upload

---
+++ tetris_aoc.asm
@@ -0,0 +1,9 @@
+.data
+	corblocoI:		.word 0x0000ffff		#Cor do bloco I: Azul claro
+	corblocoJ:		.word 0x000000cd		#Cor do bloco J: Azul escuro
+	corblocoL:		.word 0x00ffa500		#Cor do bloco L: Laranja
+	corblocoO:		.word 0x00ffff00		#Cor do bloco O: Amarelo
+	corblocoS:		.word 0x0000ff00		#Cor do bloco S: Verde
+	corblocoT:		.word 0x009400d3		#Cor do bloco T: Roxo
+	corblocoZ:		.word 0x00ff0000		#Cor do bloco Z: Vermelho
+	corMargem:		.word 0x00ffffff		#Cor da Margem: Branco
