4b0ccabe8d20ed1f93ed69c783454b648c739017
pedronaresi <pmnaresi@hotmail.com>
2016-12-02 11:17:48 -0200

    Dados das pecas

--- pong.asm
+++ pong.asm
@@ -22,247 +22,247 @@ NewGame:
 	jal ClearBoard
 
 	Lines:
-		li $a0, 10 
-		li $a1, 13 
+		li $a0, 10
+		li $a1, 13
 		lw $a2, colorTwo
-		li $a3, 18 
+		li $a3, 18
 		jal DrawHorizontalLine
-		
+
 		li $a1, 14
 		jal DrawHorizontalLine
-		
+
 		li $a0, 46
 		li $a3, 54
 		jal DrawHorizontalLine
-		
+
 		li $a1, 13
 		jal DrawHorizontalLine
-		
+
 		li $a0, 10
 		li $a1, 15
 		lw $a2, blueColor
-		li $a3, 18 
+		li $a3, 18
 		jal DrawHorizontalLine
-		
-		li $a1, 16 
+
+		li $a1, 16
 		jal DrawHorizontalLine
-		
+
 		li $a0, 46
-		li $a3, 54 
+		li $a3, 54
 		jal DrawHorizontalLine
-		
+
 		li $a1, 15
 		jal DrawHorizontalLine
-		
+
 		li $a0, 10
 		li $a1, 17
 		lw $a2, colorOne
 		li $a3, 18
 		jal DrawHorizontalLine
-		
+
 		li $a1, 18
 		jal DrawHorizontalLine
-		
+
 		li $a0, 46
 		li $a3, 54
 		jal DrawHorizontalLine
-		
+
 		li $a1, 17
-		jal DrawHorizontalLine	
-		
+		jal DrawHorizontalLine
+
 	PONG:
 		li $a0, 21
 		li $a1, 13
 		lw $a2, ballColor
 		li $a3, 18
 		jal DrawVerticalLine
-		
+
 		li $a0, 27
 		jal DrawVerticalLine
-		
+
 		li $a0, 31
 		jal DrawVerticalLine
-		
+
 		li $a0, 33
 		jal DrawVerticalLine
-		
+
 		li $a0, 37
 		jal DrawVerticalLine
-		
+
 		li $a0, 39
 		jal DrawVerticalLine
-		
-		li $a0, 34 
+
+		li $a0, 34
 		li $a1, 13
 		li $a3, 14
 		jal DrawVerticalLine
-	
+
 		li $a0, 35
 		li $a1, 15
 		li $a3, 16
 		jal DrawVerticalLine
-		
+
 		li $a0, 25
 		li $a1, 14
 		jal DrawVerticalLine
-		
+
 		li $a0, 22
 		li $a1, 16
 		li $a3, 24
 		jal DrawHorizontalLine
-	
+
 		li $a0, 22
 		li $a1, 16
 		li $a3, 25
 		jal DrawHorizontalLine
-	
+
 		li $a1, 13
 		jal DrawHorizontalLine
-		
+
 		li $a0, 27
 		li $a3, 30
 		jal DrawHorizontalLine
-	
+
 		li $a1, 18
 		jal DrawHorizontalLine
-		
+
 		li $a0, 40
 		li $a3, 43
 		jal DrawHorizontalLine
-		
+
 		li $a1, 13
 		jal DrawHorizontalLine
-		
+
 		li $a0, 41
 		li $a1, 16
 		jal DrawHorizontalLine
-		
+
 		li $a0, 36
 		li $a1, 17
 		jal DrawPoint
-		
+
 		li $a0, 43
 		jal DrawPoint
-	
+
 	Press1or2:
 		li $a0, 35
 		li $a1, 25
 		li $a3, 29
 		jal DrawVerticalLine
-		
+
 		li $a0, 17
 		jal DrawVerticalLine
-		
+
 		li $a0, 19
 		jal DrawVerticalLine
-		
+
 		li $a0, 21
 		jal DrawVerticalLine
-		
+
 		li $a0, 39
 		jal DrawVerticalLine
-		
+
 		li $a0, 41
 		jal DrawVerticalLine
-		
+
 		li $a0, 43
 		jal DrawVerticalLine
-		
+
 		li $a0, 45
 		jal DrawVerticalLine
-		
+
 		li $a0, 12
 		jal DrawVerticalLine
-		
+
 		li $a3, 15
 		jal DrawHorizontalLine
-		
+
 		li $a1, 27
 		jal DrawHorizontalLine
-		
+
 		li $a0, 15
 		li $a1, 26
 		li $a3, 27
 		jal DrawVerticalLine
-		
+
 		li $a0, 18
 		li $a1, 25
 		li $a3, 27
 		jal DrawVerticalLine
-		
+
 		li $a0, 22
 		li $a1, 27
 		jal DrawPoint
-		
+
 		li $a1, 29
 		jal DrawPoint
-		
+
 		li $a1, 25
 		jal DrawPoint
-		
-		li $a0, 24 
+
+		li $a0, 24
 		li $a1, 29
 		li $a3, 26
 		jal DrawHorizontalLine
-		
+
 		li $a0, 24
 		li $a1, 27
 		jal DrawHorizontalLine
-		
+
 		li $a1, 25
 		jal DrawHorizontalLine
-		
+
 		li $a1, 26
 		jal DrawPoint
-		
+
 		li $a0, 26
 		li $a1, 28
 		jal DrawPoint
-		
+
 		li $a0, 22
 		li $a1, 25
 		jal DrawPoint
-		
+
 		li $a0, 34
 		li $a1, 26
 		jal DrawPoint
-		
+
 		li $a1, 29
 		li $a3, 36
 		jal DrawHorizontalLine
 
 		li $a0, 40
 		jal DrawPoint
-		
+
 		li $a1, 25
 		jal DrawPoint
 
 		li $a0, 44
 		li $a3, 27
 		jal DrawVerticalLine
-			
+
 		li $a0, 49
 		li $a1, 25
 		li $a3, 50
 		jal DrawHorizontalLine
-	
+
 		li $a1, 28
 		jal DrawPoint
-	
+
 		li $a0, 50
 		li $a1, 29
 		jal DrawPoint
-		
+
 		li $a1, 27
 		jal DrawPoint
-	
+
 		li $a0, 51
 		li $a1, 26
 		jal DrawPoint
-	
+
 		li $a0, 48
 		jal DrawPoint
-		
+
 		li $a1, 29
 		li $a3, 51
 		jal DrawHorizontalLine
@@ -270,48 +270,48 @@ NewGame:
 		li $a0, 28
 		li $a3, 30
 		jal DrawHorizontalLine
-	
+
 		li $a1, 27
 		jal DrawHorizontalLine
-		
+
 		li $a1, 25
 		li $a3, 30
 		jal DrawHorizontalLine
-	
+
 		li $a1, 26
 		jal DrawPoint
-	
+
 		li $a0, 30
 		li $a1, 28
 		jal DrawPoint
-		
+
 		li $a1, 26
 		lw $a2, backgroundColor
 		jal DrawPoint
-		
+
 		li $a0, 19
 		li $a1, 27
 		jal DrawPoint
-		
+
 		li $a0, 45
 		jal DrawPoint
-		
+
 		li $a0, 44
 		li $a1, 26
 		jal DrawPoint
-		
+
 		li $a0, 18
 		jal DrawPoint
-		
+
 SelectMode:
 		lw $t1, 0xFFFF0004		# check to see which key has been pressed
 		beq $t1, 0x00000031, SetOnePlayerMode # 1 pressed
 		beq $t1, 0x00000032, SetTwoPlayerMode # 2 pressed
-		
+
 		li $a0, 250	#
 		li $v0, 32	# pause for 250 milisec
 		syscall		#
-		
+
 		j SelectMode    # Jump back to the top of the wait loop
 		
 SetOnePlayerMode:
@@ -322,14 +322,14 @@ SetTwoPlayerMode:
 BeginGame:
 		sw $zero, 0xFFFF0000		# clear the button pushed bit
 		sw $t1, mode
-		
-		
+
+
 
 # $s0 stores p1 dir
 # $s1 stores p2 dir
 # $s2 stores balls x-velocity-count
 # $s3 stores balls y-velocity-count
-# $s4 stores paddle one's position, 
+# $s4 stores paddle one's position,
 # $s5 stores paddle two's position
 # $s6 stores the balls x position
 # $s7 stores the balls y position
@@ -342,7 +342,7 @@ NewRound:
 		sw $t1, yDir
 		sw $zero, compSpeed 	# reset compCount and compSpeed to 0 for first collision
 		sw $zero, compCount
-		
+
 		li $s0, 0 	# 0x01000000 up; 0x02000000 down; 0 stay
 		li $s1, 0	# 0x01000000 up; 0x02000000 down; 0 stay
 		lw $s2, xDir	# wait this long before you move over 1 x
@@ -353,19 +353,19 @@ NewRound:
 		li $s7, 0
 
 		jal ClearBoard
-		
+
 		lw $a2, P1Score
 		li $a3, 1
 		jal DrawScore
 		lw $a2, P2Score
 		li $a3, 54
 		jal DrawScore
-		
+
 		li $a0, 13
 		move $a1, $s4
 		lw $a2, colorOne
 		jal DrawPaddle
-		
+
 		li $a0, 50
 		move $a1, $s5
 		lw $a2, colorTwo
@@ -380,7 +380,7 @@ DrawObjects:
 		move $a1, $s7
 		jal CheckForCollisions
 		jal MoveBall
-		
+
 		li $a0, 13
 		move $a1, $s4
 		lw $a2, colorOne
@@ -388,15 +388,15 @@ DrawObjects:
 		jal DrawPaddle
 		move $s4, $a1	# a1 has the new top position stored
 		move $s0, $a3	# a3 has the new direction stored if it hit an edge
-		
-		li $a0, 50		
+
+		li $a0, 50
 		move $a1, $s5
 		lw $a2, colorTwo
 startAi:
 		lw $t1, mode
 		bne $t1, 1, endAi 	# branches past the Ai if 2 player mode
-		
-		lw $t0, compCount 	
+
+		lw $t0, compCount
 		addi $t0, $t0, -1	# decrement compCount
 		sw $t0, compCount
 		bgt $t0, 0, endAi
@@ -405,37 +405,37 @@ startAi:
 		addi $t1, $s5, 2	# calculate the middle of the paddle
 		blt $t1, $s7, goDown	# if bally above paddlemid, dir = 0x01000000
 		li $s1, 0x01000000
-		j endAi	
-goDown: 
+		j endAi
+goDown:
 		li $s1, 0x02000000
 endAi:
 		move $a3, $s1
 		jal DrawPaddle
 		move $s5, $a1	# a1 has the new top position stored
 		move $s1, $a3	# a3 has the new direction stored if it hit an edge
-		
-		
+
+
 # Wait and read buttons
-Begin_standby:	
+Begin_standby:
 		li $t0, 0x00000002			# load 25 into the counter for a ~50 milisec standby
-	
+
 Standby:
 		blez $t0, EndStandby
 		li $a0, 10	#
 		li $v0, 32	# pause for 10 milisec
 		syscall		#
-		
+
 		addi $t0, $t0, -1 		# decrement counter
-		
+
 		lw $t1, 0xFFFF0000		# check to see if a key has been pressed
 		blez $t1, Standby
-				
+
 		jal AdjustDir			# see what was pushed
 		sw $zero, 0xFFFF0000		# clear the button pushed bit
 		j Standby
-EndStandby:		
+EndStandby:
 		j DrawObjects
-		
+
 # $a0 contains the paddles x position
 # $a1 contains paddles y-top position
 # $a2 contains paddle color
@@ -461,7 +461,7 @@ DrawPaddle:
    		addi $sp, $sp, 4	# change stack back
    		move $a1, $t1	# put back top y position
    		move $a2, $t2	# put back color
-   		
+
 		# move top y up (as long as its not at the top)
 		beq $a1, 0, NoMove
 		addi $a1, $a1, -1
@@ -476,7 +476,7 @@ DrawPaddle:
 		lw $ra, 0($sp)		# put return back
    		addi $sp, $sp, 4	# change stack back
    		move $a2, $t1	# put back color
-   		
+
 		# move down top y (as long as bottom is not at bottom)
 		beq $a1, 26, NoMove	# height is 31 - 5 = 26
 		addi $a1, $a1, 1
@@ -489,43 +489,43 @@ DrawPaddle:
 	StartPLoop:
 		subi $t0, $t0, 1
 		addu $t1, $a1, $t0
-		
+
 		# Converts to memory address
 		sll $t1, $t1, 6   # multiply y-coordinate by 64 (length of the field)
 		addu $v0, $a0, $t1
 		sll $v0, $v0, 2
 		addu $v0, $v0, $gp
-		
+
 		sw $a2, ($v0)
 		beqz $t0, EndPLoop
 		j StartPLoop
-	EndPLoop:		
+	EndPLoop:
 		jr $ra
 
 # $a2 contains the score of the player
 # $a3 contains the column of the leftmost scoring dot.
-# Using this information, draws along the top of the screen to display a player's score	
+# Using this information, draws along the top of the screen to display a player's score
 DrawScore:
 		addi $sp, $sp, -12	# Stores regiester values to the stack
    		sw $ra, 0($sp)
    		sw $s2, 4($sp)
    		sw $a2, 8($sp)
-   		
+
    		move $s2, $a2
    		lw $a2, ballColor
    		ble $s2, 5, DrawScoreRow1
    	DrawScoreRow2:			# Draws any score values along the second row
-   	
+
    		sub $t1, $s2, 6
    		sll $t1, $t1, 1
    		add $a0, $t1, $a3
    		li $a1, 3
    		jal DrawPoint
-   		
+
    		addi $s2, $s2 -1
-   		
+
    		bge $s2, 6, DrawScoreRow2
-   		
+
 	DrawScoreRow1:			# Draws any score values along the first row
 		beq $s2, $zero, DrawScoreEnd
 		sub $t1, $s2, 1
@@ -533,22 +533,22 @@ DrawScore:
    		add $a0, $t1, $a3
    		li $a1, 1
    		jal DrawPoint
-   		
+
    		addi $s2, $s2, -1
-   		
+
    		j DrawScoreRow1
-	
+
 	DrawScoreEnd:
 		lw $ra, 0($sp)		# restores register values from the stack
 		lw $s2, 4($sp)
 		lw $a2, 8($sp)
    		addi $sp, $sp, 12
-		
+
 		jr $ra
-		
+
 # $a0 contains x position
-# $a1 contains y position	
-MoveBall:		
+# $a1 contains y position
+MoveBall:
 		# draw over the last point
 		lw $a2, backgroundColor
 		addi $sp, $sp, -4
@@ -556,31 +556,31 @@ MoveBall:
 		jal DrawPoint
 		lw $ra, 0($sp)		# put return back
    		addi $sp, $sp, 4	# change stack back
-   		
+
    		add $s6, $s6, $s2	# add the x velocity to the x coord
    		# y doesnt always change, check if it needs to
    		addi $s3, $s3, -1
    		bgt $s3, 0, NoYChange
 ChangeY:
-		lw $t0, yDir	
+		lw $t0, yDir
 		add $s7, $s7, $t0
 		lw $s3, ySpeed
 NoYChange:
    		# do nothing
-   		
+
    		# draw the new loc
 		move $a0, $s6
 		move $a1, $s7
 		lw $a2, ballColor
-		
-# $a0 contains x position, $a1 contains y position, $a2 contains the color	
+
+# $a0 contains x position, $a1 contains y position, $a2 contains the color
 DrawPoint:
 		sll $t0, $a1, 6   # multiply y-coordinate by 64 (length of the field)
 		addu $v0, $a0, $t0
 		sll $v0, $v0, 2
 		addu $v0, $v0, $gp
 		sw $a2, ($v0)		# draw the color to the location
-		
+
 		jr $ra
 
 # $a0 the x starting coordinate
@@ -588,26 +588,26 @@ DrawPoint:
 # $a2 the color
 # $a3 the x ending coordinate
 DrawHorizontalLine:
-		
+
 		addi $sp, $sp, -4
    		sw $ra, 0($sp)
-		
+
 		sub $t9, $a3, $a0
 		move $t1, $a0
-		
+
 	HorizontalLoop:
-		
+
 		add $a0, $t1, $t9
 		jal DrawPoint
 		addi $t9, $t9, -1
-		
+
 		bge $t9, 0, HorizontalLoop
-		
+
 		lw $ra, 0($sp)		# put return back
    		addi $sp, $sp, 4
 
 		jr $ra
-		
+
 # $a0 the x coordinate
 # $a1 the y starting coordinate
 # $a2 the color
@@ -616,32 +616,32 @@ DrawVerticalLine:
 
 		addi $sp, $sp, -4
    		sw $ra, 0($sp)
-		
+
 		sub $t9, $a3, $a1
 		move $t1, $a1
-		
+
 	VerticalLoop:
-		
+
 		add $a1, $t1, $t9
 		jal DrawPoint
 		addi $t9, $t9, -1
-		
+
 		bge $t9, 0, VerticalLoop
-		
+
 		lw $ra, 0($sp)		# put return back
    		addi $sp, $sp, 4
-   		
+
 		jr $ra
-		
+
 
 # AdjustDir  changes the players direction registers depending on the key pressed
-AdjustDir: 
+AdjustDir:
 		lw $a0, 0xFFFF0004		# Load button pressed
-		
+
 AdjustDir_left_up:
 		bne $a0, 97, AdjustDir_left_down  # a
 		li $s0, 0x01000000	# up
-		j AdjustDir_done		
+		j AdjustDir_done
 
 AdjustDir_left_down:
 		bne $a0, 122, AdjustDir_right_up	# z
@@ -678,7 +678,7 @@ LeftCollision:
 		sub $t3, $s7, $s4		# store distance from top to hit
 		li $s2, 1			# change x-dir
 		j PaddleHit
-   		
+
 NoLeftCollision:
 		bne $s6, 49, NoPaddleCollision	# see if it is in the right-paddle collision section
 RightCollision:
@@ -687,27 +687,27 @@ RightCollision:
 		bgt $s7, $t3, NoPaddleCollision	# if it is below, there is no vertical collision
 		sub $t3, $s7, $s5		# store distance from top to hit
 		li $s2, -1			# change x-dir
-		j PaddleHit		
+		j PaddleHit
 
 NoPaddleCollision:
 		j CheckHorizontalHit
-		
-PaddleHit: 
+
+PaddleHit:
 		addi $sp, $sp, -8
    		sw $a0, 0($sp)   	# arguments on stack
    		sw $a1, 4($sp)
-		
+
 		li $a0, 80		# Make the sound when the ball hits the paddle
 		li $a1, 80
 		li $a2, 32
 		li $a3, 127
 		li $v0, 31
 		syscall
-		
+
    		lw $a0, 0($sp)   	# Puts arguments back in their registers for later use
    		lw $a1, 4($sp)
    		addi $sp, $sp, 8
-		
+
 		lw $t4, Level		# set the compSpeed here so it never misses the first ball
 		sw $t4, compSpeed
 		beq $t3, 0, tophigh
@@ -751,21 +751,21 @@ bottomlow:
 		sw $s3, ySpeed
 		li $s3, 1
 		sw $s3, yDir
-		
+
 CheckHorizontalHit:
 		beq $s7, 31, HorizontalWallHit
 		bne $s7, 0, NoCollision
-		
-HorizontalWallHit: 
+
+HorizontalWallHit:
 		# play a sound
 		addi $sp, $sp, -8
    		sw $a0, 0($sp)   	# arguments on stack
    		sw $a1, 4($sp)
-		
+
    		lw $a0, 0($sp)   	# Puts arguments back in their registers for later use
    		lw $a1, 4($sp)
    		addi $sp, $sp, 8
-   		
+
 		# change y direction if y-count=1 (prevents it from switching until y is about to change)
 		bgt $s3, 1, NoCollision
 		lw $t4, yDir
@@ -787,32 +787,32 @@ ClearBoard:
 		j StartCLoop
 	EndCLoop:
 		jr $ra
-		
+
 POneRoundLoss:
 		# Increment player 2's score
 		lw $t1, P2Score
 		addi $t1, $t1, 1
 		sw $t1, P2Score
-		
+
 		#Ready the next round
 		li $t2, 1
 		sw $t2, xDir
-		
+
 		li $a3, 54
-		sw $zero, 0xFFFF0004  # Zeros the key press 
+		sw $zero, 0xFFFF0004  # Zeros the key press
 		beq $t1, 10, EndGame
-		
+
 		j PlayPointSound
-PTwoRoundLoss:	
+PTwoRoundLoss:
 		# Increment player 1's score
 		lw $t1, P1Score
 		addi $t1, $t1, 1
 		sw $t1, P1Score
-		
+
 		#Ready the next round
 		li $t2, -1
 		sw $t2, xDir
-		
+
 		li $a3, 1
 		sw $zero, 0xFFFF0004 # Zeros the key press
 		beq $t1, 10, EndGame
@@ -825,99 +825,99 @@ PlayPointSound:
 		li $a3, 127
 		li $v0, 31
 		syscall
-   		
+
    		j NewRound
-	
+
 # Ends the game, wrapping up the process
 EndGame:
 		jal ClearBoard
-		
+
 		lw $t0, P1Score
 		bne $t0, 10, WinTwo
-		
-		
+
+
 	WinOne:	li $a0, 34 #the x coordinate
 		li $a1, 12 #the y starting coordinate
 		lw $a2, ballColor #the color
 		li $a3, 15 #the y ending coordinate
 		jal DrawVerticalLine
-		
+
 		li $a0, 33
 		li $a1, 13
 		jal DrawPoint
-		
+
 		li $a1, 16 #the y coordinate
 		li $a3, 35 #the x ending coordinate
 		jal DrawHorizontalLine
-		
+
 		j WinP
-		
+
 	WinTwo:	li $a0, 33 #the x starting coordinate
 		li $a1, 16 #the y coordinate
 		lw $a2, ballColor #the color
 		li $a3, 36 #the x ending coordinate
 		jal DrawHorizontalLine
-	
+
 		li $a0, 34 #the x starting coordinate
 		li $a1, 12 #the y coordinate
 		li $a3, 35 #the x ending coordinate
 		jal DrawHorizontalLine
-	
+
 		li $a1, 15
 		jal DrawPoint
-	
+
 		li $a0, 35
 		li $a1, 16
 		jal DrawPoint
-	
+
 		li $a1, 14
 		jal DrawPoint
-	
+
 		li $a0, 36
 		li $a1, 13
 		jal DrawPoint
-	
+
 		li $a0, 33
 		jal DrawPoint
-		
+
 	WinP:	li $a0, 27 #the x coordinate
 		li $a1, 12 #the y starting coordinate
 		li $a3, 16 #the y ending coordinate
 		jal DrawVerticalLine
-		
+
 		li $a0, 30 #the x coordinate
 		li $a3, 14 #the ending y coordinate
 		jal DrawVerticalLine
-		
+
 		li $a0, 28 #the x starting coordinate
 		li $a3, 29 #the x ending coordinate
 		jal DrawHorizontalLine
-	
+
 		li $a1, 14 #the y coordinate
 		jal DrawHorizontalLine
 
 		li $a0, 100 	#
 		li $v0, 32	# Pause for 100 milisec
 		syscall		#
-		
+
 		sw $zero, 0xFFFF0000
 
-WaitForReset:		
+WaitForReset:
 		li $a0, 10 	#
 		li $v0, 32	# pause for 10 milisec
 		syscall		#
-		
+
 		lw $t0, 0xFFFF0000
 		beq $t0, $zero, WaitForReset
-		
+
 		j Reset
-		
-Reset:		
+
+Reset:
 		sw $zero, P1Score
 		sw $zero, P2Score
 		sw $zero, 0xFFFF0000	# Zeros the keypress words in memory
 		sw $zero, 0xFFFF0004
-		
+
 		jal ClearBoard
-		
+
 		j NewGame
